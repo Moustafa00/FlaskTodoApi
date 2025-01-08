62c246fbc52283ddcaba6fb2656ab0020d4d92e1
Moustafa Jarjour <mkj194@student.bau.edu.lb>
2025-01-08 20:45:24 +0200

    Bind app to PORT environment variable for Render

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify, request
 
 app = Flask(__name__)
@@ -19,3 +20,7 @@ def delete_todo(todo_id):
     global todos
     todos = [todo for todo in todos if todo['id'] != todo_id]
     return jsonify({'message': 'Todo deleted!'})
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))  
+    app.run(host='0.0.0.0', port=port)
