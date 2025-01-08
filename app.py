3fcc4ccb641b94f5e515332c02cfa9e6d356585f
Moustafa Jarjour <mkj194@student.bau.edu.lb>
2025-01-08 19:31:27 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+todos = []
+
+@app.route('/todos', methods=['GET'])
+def get_todos():
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_todo():
+    todo = request.json.get('task')
+    todos.append({'id': len(todos) + 1, 'task': todo})
+    return jsonify({'message': 'Todo added!'})  # Ensure this line is present
+
+@app.route('/todos/<int:todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    global todos
+    todos = [todo for todo in todos if todo['id'] != todo_id]
+    return jsonify({'message': 'Todo deleted!'})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)  
